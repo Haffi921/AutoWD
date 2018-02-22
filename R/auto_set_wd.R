1c12c8c8713c9b83b3c26a8d17601d8c4010a6d3
Haffi921 <haffi921@gmail.com>
2018-02-22 15:25:25 +0000

    Initial commit

---
+++ R/auto_set_wd.R
@@ -0,0 +1,35 @@
+#' Set working directory function
+#'
+#' This function is shorthand to set the working directory to the active document's path.
+#' It should be used as a beginning argument in a script before loading any data that's
+#' in the same folder as the script.
+#' @keywords set working directory active document path
+#' @export
+
+
+autowd <- function() {
+  ## We need two solutions:
+  ### One when user is using rStudio
+  ### and one where the user is not using rStudio
+  isRStudio <- Sys.getenv("RSTUDIO") == "1"
+
+  if (isRStudio) {
+  	## This function needs the rStudioAPI package to run
+  	if (!requireNamespace("rstudioapi", quietly = TRUE)) {
+  		stop("Package \"rstudioapi\" needed for this function to work in RStudio.
+  				 Please install it.",
+  				 call. = FALSE)
+  	}
+
+  	## Setting the the working directory
+  	## to the path of the active document
+  	fileDir <- rstudioapi::getActiveDocumentContext()$path # Get active document's path
+  	setwd(dirname(fileDir)) # Setting the working directory
+
+  	## Detaching rStudioAPI
+  	detach("package:rstudioapi")
+  }
+  else {
+    setwd(system("pwd", intern = T))
+  }
+}
