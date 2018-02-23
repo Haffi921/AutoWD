ee121ecb9a7de9ae9e9bccf2d6171aac0a872614
Haffi921 <haffi921@gmail.com>
2018-02-23 20:31:44 +0000

    Major fix. Most of the functionality that autowd() had has now been moved to 'dirtools'. Subsequently 'AutoWD' now has 'dirtools' as dependancy.

--- R/auto_wd.R
+++ R/auto_wd.R
@@ -6,45 +6,6 @@
 #' @keywords set working directory active document path
 #' @export
 
-
-# autowd <- function() {
-#   ## We need two solutions:
-#   ### One when user is using rStudio
-#   ### and one where the user is not using rStudio
-#   isRStudio <- Sys.getenv("RSTUDIO") == "1"
-#
-#   if (isRStudio) {
-#   	## This function needs the rStudioAPI package to run
-#   	if (!requireNamespace("rstudioapi", quietly = TRUE)) {
-#   		stop("Package \"rstudioapi\" needed for this function to work in RStudio.
-#   				 Please install it.",
-#   				 call. = FALSE)
-#   	}
-#
-#   	## Setting the the working directory
-#   	## to the path of the active document
-#   	fileDir <- rstudioapi::getActiveDocumentContext()$path # Get active document's path
-#   	setwd(dirname(fileDir)) # Setting the working directory
-#   }
-#   else {
-#   	switch (Sys.info()[['sysname']],
-#   		Windows = {
-#   			setwd(shell("cd", intern = T))
-#   			},
-#   		Linux = ,
-#   		Darwin = {
-#   			setwd(system("pwd", intern = T))
-#   			},
-#   		{
-#   			stop("Could not determine operating system.",
-#   					 call. = FALSE)
-#   		}
-#   	)
-#   }
-# }
-
-
-
 autowd <- function() {
 	if(!requireNamespace("dirtools", quietly = TRUE)) {
 		stop("Package \"Haffi921/dirtools\" needed for this function to work. \nTo install do:\n  library(\"devtools\")\n  install_github(\"Haffi/dirtools\")")
