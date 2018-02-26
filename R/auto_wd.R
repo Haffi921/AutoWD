8e93fcebc2425833ae998dfe6a615b98f1cb439e
Haffi921 <haffi921@gmail.com>
2018-02-26 01:43:26 +0000

    Small bug test + Added argument 'quietly' that defaults to TRUE; if FALSE 'autowd()' will display what working directory is changed to.

--- R/auto_wd.R
+++ R/auto_wd.R
@@ -6,19 +6,21 @@
 #' @keywords set working directory active document path
 #' @export
 
-autowd <- function(up_dir = 0) {
+autowd <- function(quietly = TRUE) {
 	if(!requireNamespace("dirtools", quietly = TRUE)) {
 		stop("Package \"Haffi921/dirtools\" needed for this function to work. \nTo install do:\n  library(\"devtools\")\n  install_github(\"Haffi/dirtools\")")
 	}
 
 	wd <- dirtools::get_this_dir()
 
-	while(up_dir > 0) {
-		wd <- dirname(wd)
-		up_dir <- up_dir - 1
-	}
+	# while(up_dir > 0) {
+	# 	wd <- dirname(wd)
+	# 	up_dir <- up_dir - 1
+	# }
 
-	print(paste("Changing directory to:", wd))
+	if(!quietly) {
+		print(paste("Changing directory to:", wd))
+	}
 
 	setwd(wd)
 }
